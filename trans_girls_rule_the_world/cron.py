b29081b8ee714a8e1f5f3ec620a0082456933288
Emma Foster <deafjams@gmail.com>
2016-01-03 11:38:23 -0500

    Cron path fix

--- trans_girls_rule_the_world/cron.py
+++ trans_girls_rule_the_world/cron.py
@@ -3,14 +3,16 @@ Cronjob for trans girls bot
 """
 import os
 import plan
+import trans_girls_rule_the_world
 
 def trans_girls_cron():
     """
     Config for trans girls cron
     """
     cron = plan.Plan('trans_girls_rule_the_world')
+    path = trans_girls_rule_the_world.__path__[0]
 
-    cron.script('rule.py', every='5.minutes', path=os.getcwd())
+    cron.script('rule.py', every='5.minutes', path=path)
 
     #update cron if crontab exists, write new cron if crontab doesn't exist
     try:
