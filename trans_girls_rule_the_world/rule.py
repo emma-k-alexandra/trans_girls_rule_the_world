1e965830427a4a67f5cd87fc23b48c941289d724
Emma Foster <deafjams@gmail.com>
2018-09-21 18:39:49 -0400

    Revert "Simpler id getter"
    This reverts commit 32539c791964d9ca4ed5e428054e4daba4e5b060.

--- trans_girls_rule_the_world/rule.py
+++ trans_girls_rule_the_world/rule.py
@@ -89,7 +89,11 @@ class TransGirls(object):
         Returns:
             int - The given post's root id
         """
-        return post.get('id') or post.get('root_id')
+        if not post['trail']:
+            return post['photos'][0]['original_size']['url']
+
+        post_id_list = [trail['post']['id'] for trail in post['trail'] if trail.get('is_root_item')]
+        return int(post_id_list[0])
 
 
     def already_reblogged(self, post):
