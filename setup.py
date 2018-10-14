0ff086f14064fecb0368067ba5fb5db38b3465ca
Emma Foster <deafjams@gmail.com>
2018-10-14 10:10:41 -0400

    Machine learning!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='trans_girls_rule_the_world',
-    version='6.0.0',
+    version='7.0.0',
     description='A tumblr bot to reblog trans girl\'s selfies',
     url='https://github.com/Deafjams/trans_girls_rule_the_world',
     author='Emma Foster',
@@ -14,7 +14,10 @@ setuptools.setup(
         'pytumblr>=0.0.7,<0.1.0',
         'emoji>=0.4.5,<1.0',
         'plan>=0.5,<1.0',
-        'flask>=0.12.2,<1.0'
+        'flask>=0.12.2,<1.0',
+        'numpy',
+        'scipy',
+        'scikit-learn'
     ],
     package_dir={
         'trans_girls_rule_the_world': 'trans_girls_rule_the_world'
